0a74f49b045ad108737dec99b3483aeb6b0d0df9
JuanJObanano <juanjose1234obando@gmail.com>
2026-04-25 16:57:47 +0000

    ayuda

---
+++ src/components/UsuarioCard.jsx
@@ -0,0 +1,26 @@
+import './UsuarioCard.css';
+import UsuarioCard from '../components/UsuarioCard';
+
+function UsuarioCard({ nombre, rol, estado, imagen}){
+    const claseEstado = estado === 'Activo' ? 'badge-activo' : 'badge-inactivo';
+    return(
+        <div>
+            <img src={imagen} alt={'foto de ${imagen}'} className="usuario-img" />
+            <div className="usuario-info">
+                <h3>{nombre}</h3>
+                <p className='usuario-rol1'>{rol}</p>
+                <span className={'usuario-badge ${claseEstado}'}>
+                    {estado}
+                </span>
+            </div>
+            <div className='usuario-acciones'>
+                <button onClick={ ()=> alert('Enviando mensaje a ${nombre}')}>
+                    mensaje
+                </button>
+            </div>
+        </div>
+    )
+
+}
+
+export default UsuarioCard;
--- src/pages/UsuariosLista.jsx
+++ src/pages/UsuariosLista.jsx
@@ -1,9 +1,13 @@
 import { Link } from 'react-router-dom';
+import UsuarioCard from '../components/UsuarioCard';
 
 function UsuariosLista() {
     return (
         <div>
             <h1>Lista de usuarios</h1>
+            <p>Pasando PROPS uno por uno</p>
+
+            <div style={}></div>
             <ul>
                 <li><Link to="/usuario/123">Ver perfil del usuario Juan Perez (123)</Link></li>
                 <li><Link to="/usuario/217">Ver perfil del usuario Ana Gomez (217)</Link></li>
